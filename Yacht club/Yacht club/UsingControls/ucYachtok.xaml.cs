94d0138bbc3da5cc7630c520042a26d0d01a4588
jacjozs <36637788+jacjozs@users.noreply.github.com>
2018-03-29 16:13:18 +0200

    Ablak listát frissitése

--- Yacht club/Yacht club/UsingControls/ucYachtok.xaml.cs
+++ Yacht club/Yacht club/UsingControls/ucYachtok.xaml.cs
@@ -50,59 +50,61 @@ namespace Yacht_club.UsingControls
                 Label ID = new Label();
                 ID.Content = "ID: " + Yachts[i].id;
                 ID.Style = MenuStackLabel;
+                ID.Width = 110;
 
                 Label nev = new Label();
                 nev.Content = "Név: " + Yachts[i].nev;
                 nev.Style = MenuStackLabel;
+                nev.Width = 110;
 
                 panel2.Children.Add(ID);
                 panel2.Children.Add(nev);
 
                 StackPanel panel3 = new StackPanel();
-                panel3.Width = 150;
+                panel3.Width = 180;
                 panel3.Height = 40;
 
                 Label tipus = new Label();
                 tipus.Content = "Típus: " + Yachts[i].tipus;
                 tipus.Style = MenuStackLabel;
+                tipus.Width = 180;
 
                 Label Tulaj = new Label();
                 Tulaj.Content = "Tulajdonos: " + Yachts[i].full_name;
                 Tulaj.Style = MenuStackLabel;
+                Tulaj.Width = 180;
 
                 panel3.Children.Add(tipus);
                 panel3.Children.Add(Tulaj);
 
                 StackPanel panel4 = new StackPanel();
-                panel4.Width = 150;
+                panel4.Width = 100;
                 panel4.Height = 40;
 
                 Label ferohely = new Label();
                 ferohely.Content = "Férőhely: " + Yachts[i].ferohely;
                 ferohely.Style = MenuStackLabel;
+                ferohely.Width = 100;
 
                 Label Napiar = new Label();
-                Napiar.Content = "Magas: " + Yachts[i].napi_ar;
+                Napiar.Content = "Napi ár: " + Yachts[i].napi_ar;
                 Napiar.Style = MenuStackLabel;
+                Napiar.Width = 100;
 
                 panel4.Children.Add(Napiar);
                 panel4.Children.Add(ferohely);
 
                 StackPanel panel5 = new StackPanel();
-                panel5.Width = 150;
+                panel5.Width = 120;
                 panel5.Height = 40;
                 panel5.HorizontalAlignment = HorizontalAlignment.Left;
 
                 Label kikoto = new Label();
                 kikoto.Content = "Kikötő: " + Yachts[i].kikoto;
                 kikoto.Style = MenuStackLabel;
-
-                Label Hosszu = new Label();
-                Hosszu.Content = "";
-                Hosszu.Style = MenuStackLabel;
+                kikoto.Width = 120;
 
                 panel5.Children.Add(kikoto);
-                panel5.Children.Add(Hosszu);
 
                 panel.Children.Add(panel2);
                 panel.Children.Add(panel3);
