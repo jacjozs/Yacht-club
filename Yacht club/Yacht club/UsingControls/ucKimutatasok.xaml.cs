0a24ddceff05f38540ed0191175a334a7d958f07
jacjozs <36637788+jacjozs@users.noreply.github.com>
2018-04-08 12:31:26 +0200

    Diagram teszt

--- Yacht club/Yacht club/UsingControls/ucKimutatasok.xaml.cs
+++ Yacht club/Yacht club/UsingControls/ucKimutatasok.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace Yacht_club.UsingControls
 {
@@ -8,10 +10,40 @@ namespace Yacht_club.UsingControls
     /// </summary>
     public partial class ucKimutatasok : UserControl
     {
+        private Point[] points;
         public ucKimutatasok()
         {
             InitializeComponent();
             Globals.UpdateHistory();
+            points = new Point[5];
+            points[0].X = 0;
+            points[0].Y = 164;
+
+            points[1].X = 75;
+            points[1].Y = 200;
+
+            points[2].X = 150;
+            points[2].Y = 150;
+
+            points[3].X = 225;
+            points[3].Y = 220;
+
+            points[4].X = 300;
+            points[4].Y = 100;
+
+            Polyline line = new Polyline();
+            PointCollection collection = new PointCollection();
+            foreach (Point pont in points)
+            {
+                collection.Add(pont);
+            }
+            line.Stretch = Stretch.Fill;
+            line.Points = collection;
+            line.VerticalAlignment = VerticalAlignment.Center;
+            line.HorizontalAlignment = HorizontalAlignment.Left;
+            line.Stroke = Globals.MainTheme.uc_hatter;
+            line.StrokeThickness = 15;
+            GridDiagrams.Children.Add(line);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
--- Yacht club/Yacht club/UsingControls/ucUzenet.xaml.cs
+++ Yacht club/Yacht club/UsingControls/ucUzenet.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace Yacht_club.UsingControls
 {
@@ -23,7 +10,6 @@ namespace Yacht_club.UsingControls
         public ucUzenet()
         {
             InitializeComponent();
-            labelid.Content = Globals.selectedMessage.uzenet_id;
             Globals.UpdateHistory();
         }
     }
